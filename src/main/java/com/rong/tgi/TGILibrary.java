318e33f4cfe8059f38eb5cc1283c3a693d5195b2
Ethan Mahmood <ethanmahmood@gmail.com>
2018-06-07 02:13:57 +0800

    Experimental.

--- src/main/java/com/rong/tgi/TGILibrary.java
+++ src/main/java/com/rong/tgi/TGILibrary.java
@@ -5,10 +5,14 @@ import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.rong.tgi.dimension.TeleportToNetherEventHandler;
+import com.rong.tgi.dimension.BlockNetherEntrance;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
@@ -16,7 +20,12 @@ import net.minecraftforge.fml.common.event.FMLConstructionEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
 import net.minecraftforge.fml.common.toposort.ModSorter;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.registries.IForgeRegistry;
 
 @Mod(modid = TGILibrary.MODID, name = TGILibrary.MODNAME, version = TGILibrary.VERSION, useMetadata = true)
 
@@ -31,9 +40,17 @@ public class TGILibrary {
     public static TGILibrary instance;
 	public static Logger logger;
 	
-	public static final String MODID = "tgi_library";
+	@ObjectHolder("nether_portal")
+	public static BlockNetherEntrance nether_portal;
+	
+	@SideOnly(Side.CLIENT)
+    public static void initModels() {
+		nether_portal.initModel();
+	}
+	
+	public static final String MODID = "tgilibrary";
 	public static final String MODNAME = "TGI Library";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	
 	@Mod.EventHandler
 	public void construction(FMLConstructionEvent event) {
@@ -50,7 +67,17 @@ public class TGILibrary {
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent e) {
-    	MinecraftForge.EVENT_BUS.register(new TeleportToNetherEventHandler());
+    	//MinecraftForge.EVENT_BUS.register(new TeleportToNetherEventHandler());
+    }
+    
+    @SubscribeEvent
+    public static void registerBlocks(RegistryEvent.Register<Block> event) {
+    	event.getRegistry().register(new BlockNetherEntrance().setUnlocalizedName("nether_portal"));
+    }
+    
+    @SubscribeEvent
+    public static void registerItems(RegistryEvent.Register<Item> event) {
+        event.getRegistry().register(new ItemBlock(TGILibrary.nether_portal).setRegistryName("nether_portal"));
     }
 }
 
